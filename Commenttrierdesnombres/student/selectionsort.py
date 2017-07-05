76e0a7dcfa2d9b2dd990474cdcac3a4a05f31955
naitali <brandon.naitali@student.uclouvain.be>
2017-07-05 14:06:02 +0200

    backup inginginious de secours

--- Commenttrierdesnombres/student/selectionsort.py
+++ Commenttrierdesnombres/student/selectionsort.py
@@ -15,14 +15,14 @@ if __name__ == "__main__":
     exemple=[3,1]
     trier_par_selection(exemple)
     if(exemple==[3,1]):
-        print('vous êtes obligé de changer les cartes de place dans le paquet; vous décidez de vérifier votre bloc bleu clair et les changements de la carte z.', end='', flush=True)
+        print('vous êtes obligé de changer les cartes de place dans le paquet; vous décidez de vérifier votre bloc bleu clair.', end='', flush=True)
         exit(0)
         
     # Est-ce qu'on echange bien?
     exemple=[1,3,2]
     trier_par_selection(exemple)
     if(exemple==[3,1,2] or exemple==[3,2,1] or exemple==[2,3,1] or exemple==[2,1,3]):
-        print('vous n\'échangez pas bien les bonnes cartes; vous verifiez le bloc bleu clair et les changements de la carte z', end='', flush=True)
+        print('vous n\'échangez pas bien les bonnes cartes; vous verifiez le bloc bleu clair.', end='', flush=True)
         exit(0)
       
    # Est-ce qu'on echange bien?
