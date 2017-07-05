5f42ec826a3c61339d7f9dfde78ee1331e0b21b7
Vinsifroid <duchene113@gmail.com>
2017-07-05 15:47:46 +0200

    ajout tache sur conditions

---
+++ points_interro/public/blocks.js
@@ -0,0 +1,63 @@
+Blockly.Blocks['confisquation'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Maman confisque le téléphone");
+    this.setPreviousStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['glace'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Maman offre une glace");
+    this.setPreviousStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['content'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Maman est contente");
+    this.setPreviousStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['points'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("points");
+    this.setOutput(true, "Number");
+    this.setColour(330);
+    this.setTooltip('Les points que Michel reçoit');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Python['confisquation'] = function(block) {
+  var code = 'confisquer()\n';
+  return code;
+};
+
+Blockly.Python['glace'] = function(block) {
+  var code = 'donner_glace()\n';
+  return code;
+};
+
+Blockly.Python['content'] = function(block) {
+  var code = 'maman_contente()\n';
+  return code;
+}
+
+Blockly.Python['points'] = function(block) {
+  var code = 'point()';
+  return [code, Blockly.Python.ORDER_NONE];
+}
---
+++ points_interro/student/studentmodule.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+# Code to be verified
+
+points_interro = 12
+a_moins_10 = False
+a_entre_10_et_16 = False
+a_plus_16 = False
+
+'''
+    @pre p != None
+    @post met les points_interro à p et les conditions à false
+'''
+def set_cond_test(p):
+    global points_interro, a_moins_10, a_entre_10_et_16, a_plus_16
+    points_interro = p
+    a_moins_10 = False
+    a_entre_10_et_16 = False
+    a_plus_16 = False
+
+def donner_glace():
+    global a_plus_16
+    a_plus_16 = True
+
+def confisquer():
+    global a_moins_10
+    a_moins_10 = True
+
+def maman_contente():
+    global a_entre_10_et_16
+    a_entre_10_et_16 = True
+
+def point():
+    global points_interro
+    return points_interro
+
+def student_code():
+@    @interro@@
+    return {"a_moins_10": a_moins_10, "a_entre_10_et_16": a_entre_10_et_16, "a_plus_16": a_plus_16}
+
+if __name__ == "__main__":
+    pass_test = True
+    # On teste sur les 20 valeurs possibles
+    for i in range(0,21):
+        set_cond_test(i)
+        ret = student_code()
+        #Trois cas possibles, michel a eu - de 10, l'étudiant a eu entre 10 et 16 et l'étudiant a + de 16
+        if ret == {"a_moins_10": True, "a_entre_10_et_16": False, "a_plus_16": False} and points_interro < 10:
+            pass
+        elif ret == {"a_moins_10": False, "a_entre_10_et_16": True, "a_plus_16": False} and points_interro < 17 and 9 < points_interro :
+            pass
+        elif ret == {"a_moins_10": False, "a_entre_10_et_16": False, "a_plus_16": True} and 16 < points_interro:
+            pass
+        else:
+            #si un test échoue, on ne print pas True
+            pass_test=False
+            print('Ton programme ne fonctionne si Michel a eu ' + str(i) + "\n")
+    if pass_test:
+        print('True')
