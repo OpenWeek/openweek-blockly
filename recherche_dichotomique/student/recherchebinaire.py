5254fc18140b45390deda2c6d02a0fa3ccb9c57f
naitali <brandon.naitali@student.uclouvain.be>
2017-07-07 10:54:17 +0200

    Licence AGPL sur Recherche dicho

--- recherche_dichotomique/student/recherchebinaire.py
+++ recherche_dichotomique/student/recherchebinaire.py
@@ -1,4 +1,19 @@
 #!/bin/python3
+#
+#  Copyright (c)  2017 Brandon Naitali
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # Code to be verified
 import os
 import subprocess
@@ -14,14 +29,14 @@ if __name__ == "__main__":
     exemple=[1,3,5]
     
     if(recherche_binaire(exemple,10)):
-        print('vous vous rendez compte que vous ne dites pas quand un mot est absent du dictionnaire! Vous vérifiez si vous signalez un mot absent d\'un dictionnaire contenant plusieurs mots.', end='', flush=True)
+        print('vous vous rendez compte que vous ne gérez pas le cas où le mot ne se trouve pas dans le dictionnaire!.', end='', flush=True)
         exit(0)
        
 	# 
     exemple1=[1,3]
     
     if(not recherche_binaire(exemple1,3)):
-        print('vous vous rendez compte que vous ne prenez pas le cas où vous trouvez le mot dans le dictionnaire!', end='', flush=True)
+        print('vous vous rendez compte que vous ne gérez pas le cas où vous trouvez le mot dans le dictionnaire!', end='', flush=True)
         exit(0)
     #
     exemple2=[1,2,3,4,5,6,7,8,9,10]
